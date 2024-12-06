fac3c88a35d8187e7e560ae3d5d3d618486e82ad
Martin Arlt <martin.arlt@compass-it.ch>
2024-12-06 09:43:53 +0100

    updated mermaid to 11.4.1

--- mermaid.ts
+++ mermaid.ts
@@ -4,7 +4,7 @@ export function widget(
   return {
     html: `<pre class="mermaid">${bodyText.replaceAll("<", "&lt;")}</pre>`,
     script: `
-    loadJsByUrl("https://cdn.jsdelivr.net/npm/mermaid@10.9.3/dist/mermaid.min.js","sha256-Wo7JGCC9Va/vBJBoSJNpkQ5dbOcMgQOVLyfinT526Lw=").then(() => {
+    loadJsByUrl("https://cdn.jsdelivr.net/npm/mermaid@11.4.1/dist/mermaid.min.js","sha256-a43bc1afd446f9c4cc66ac5dd45d02e8d65e26fc5344ec0ef787f88d6ddb6f9e").then(() => {
       mermaid.init().then(updateHeight);
     });
     document.addEventListener("click", () => {
